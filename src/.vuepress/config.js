e448db5fdb7acf3d05cde7e6d8bfdc7263c601f4
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-21 17:25:22 +0800

    chore: update config.js to add favicon

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -24,6 +24,35 @@ module.exports = {
       "meta",
       { name: "apple-mobile-web-app-status-bar-style", content: "black" },
     ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "../static/favicon/apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "../static/favicon/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "../static/favicon/favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "manifest", href: "../static/favicon/site.webmanifest" }],
+    ["link", { rel: "shortcut icon", href: "../static/favicon/favicon.ico" }],
+    ["meta", { name: "msapplication-TileColor", content: "#d71440" }],
   ],
 
   /**
